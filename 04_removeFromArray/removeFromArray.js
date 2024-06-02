27944209b1aa4c868f86533c6a2946f65a8f65d2
piyush kumar <77287090+Piyush-kumar-310@users.noreply.github.com>
2024-06-02 18:20:56 +0530

    finished exercise 04-removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    let array2 = [];
+    for(let i = 0; i<array.length; i++){
+        if(args.includes(array[i])){
+            continue;
+        }
+        else{
+            array2.push(array[i]);
+        }
+    }
+    return array2;
 };
 
 // Do not edit below this line
