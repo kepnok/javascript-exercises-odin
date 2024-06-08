be12a49cb25f1eae0da8bc9e6add5d5d1908111a
piyush kumar <77287090+Piyush-kumar-310@users.noreply.github.com>
2024-06-08 12:19:04 +0530

    completed exercise 12-findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,40 @@
-const findTheOldest = function() {
+const findTheOldest = function(obj) {
+    let array = obj.sort((a, b) => {
 
+        if(a.yearOfDeath === undefined){
+            a.yearOfDeath = new Date().getFullYear();
+        }
+        if(b.yearOfDeath === undefined){
+            b.yearOfDeath = new Date().getFullYear();
+        }
+
+        const lifespanA = a.yearOfDeath - a.yearOfBirth;
+        const lifespanB = b.yearOfDeath - b.yearOfBirth;
+    
+        if (lifespanA > lifespanB) return -1;
+        if (lifespanA < lifespanB) return 1;
+        return 0;
+    });
+    return array[0];
+    
 };
+console.log(findTheOldest([
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]));
 
 // Do not edit below this line
 module.exports = findTheOldest;
