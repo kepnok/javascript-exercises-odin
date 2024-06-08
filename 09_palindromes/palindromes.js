139770416eb088a2cb435d911e110d61e2539fdb
piyush kumar <77287090+Piyush-kumar-310@users.noreply.github.com>
2024-06-08 11:33:38 +0530

    finished exercise 09-palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let filterStr = string.split('').filter( char => {
+        const pancuation = [',', '.', '?', '!', ' '];
+        return !pancuation.includes(char);
+    }).join('');
 
+    //console.log(filterStr);
+
+    let lowerStr = filterStr.toLowerCase();
+    //console.log(lowerStr);
+
+    let reversed = lowerStr.split('').reverse().join('');
+    //console.log(reversed);
+    return (reversed === lowerStr);
 };
+palindromes('A car, a man, a maraca.');
 
 // Do not edit below this line
 module.exports = palindromes;
